bb2f8ad8c08026b2a2a2164582b6436e27138ab4
ONYSTUDIO <hyewon0316@gmail.com>
2021-04-01 02:32:53 +0900

    init: 플러그인 추가 및 씬 연결

--- Assets/Script/CLoginManager.cs
+++ Assets/Script/CLoginManager.cs
@@ -1,18 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System;
+using UniRx;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class CLoginManager : MonoBehaviour
 {
+
+    [SerializeField]
+    public Button m_LoginBtn;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        m_LoginBtn.OnClickAsObservable().Subscribe(_ =>
+        {
+            Debug.Log("###");
+            SceneManager.LoadScene("StageScene");
+        });
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+
 }
