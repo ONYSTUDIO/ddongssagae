39437f5ccad3f93cefbdd30ca7c1df60682326da
ONYSTUDIO <hyewon0316@gmail.com>
2021-07-21 18:24:34 +0900

    fix: 리소스교체 및 무한 스크롤 작업 중

---
+++ Assets/Script/CPlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UniRx;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CPlayerController : MonoBehaviour
+{
+    [SerializeField]
+    public Button m_JumpBtn;
+
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        m_JumpBtn.OnClickAsObservable().Subscribe(_ =>
+        {
+            Debug.Log("### Jump!!");
+            Jump();
+        });
+    }
+
+    private void Jump()
+    {
+        rb.AddForce(new Vector3(0f, 300f, 0f));
+    }
+}
